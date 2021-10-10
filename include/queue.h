999c7e783a7103a4f934b2cc18bd968ca5a69572
Danica Modino <ailara1013@gmail.com>
2021-10-10 17:21:53 +0800

    The code is done, only editing minor parts.

--- include/queue.h
+++ include/queue.h
@@ -1,16 +1,79 @@
 #ifndef QUEUE_H
 #define QUEUE_H
+#include <iostream>
+using namespace std;
 
-
-class queue
+template <class type>
+class iqueue
 {
     public:
-        queue();
-        virtual ~queue();
-
-    protected:
+        iqueue(int Size);
+        //virtual ~queue();
+        void enqueue(type value);
+        type dequeue(type &value);
+        bool isfull();
+        bool isempty();
 
     private:
+        type *quearr;
+        int rear;
+        int Front;
+        int quesize;
+        int numsize;
+
 };
+template <class type>
+iqueue<type>::iqueue(int Size)
+{
+    quearr = new type[Size];
+    quesize = Size;
+    Front = 0;
+    rear = 0;
+    numsize = 0;
+}
+
+
+template <class type>
+void iqueue<type>::enqueue(type value)
+{
+    rear++;
+    rear = rear % quesize;
+    quearr[rear] = value;
+    numsize++;
+}
+
+template <class type>
+type iqueue<type>::dequeue(type &value)
+{
+    Front++;
+    Front = Front % quesize;
+    value = quearr[Front];
+    return value;
+    numsize--;
+}
+
+template<class type>
+bool iqueue<type>::isfull()
+{
+    bool status;
+    if(numsize == quesize)
+        status = true;
+    else
+        status = false;
+
+    return status;
+}
+
+template<class type>
+bool iqueue<type>::isempty()
+{
+    bool status;
+    if(!numsize)
+        status = true;
+    else
+        status = false;
+    return status;
+}
+
 
 #endif // QUEUE_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,118 @@
 #include <iostream>
+#include <unistd.h>
+#include "queue.h"
 
 using namespace std;
 
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    string n, c;
+    int s;
+    int y;
+    if (!s)
+    {
+        cout<<"W E L C O M E   T O   R E S E R V A T I O N   P A G E"<<endl<<endl<<endl;
+        cout<<"List how many people you want to reserve in the program: ";
+        cin>>s;
+        cout<<"Loading .";
+        sleep(1);
+        cout<<" .";
+        sleep(1);
+        cout<<" .";
+        sleep(1);
+    }
+
+    iqueue<string> name(s);
+    iqueue<int> year(s);
+    iqueue<string> course(s);
+    iqueue<string> section(s);
+
+    int choice;
+
+    do
+    {   choice = 0;
+        system("cls");
+        cout<<"W E L C O M E   T O   R E S E R V A T I O N   P A G E"<<endl<<endl<<endl;
+        cout<<"[1] Add reservation"<<endl;
+        cout<<"[2] Print transcript"<<endl<<endl<<endl;
+        cout<<"Input your choice here: ";
+        cin>>choice;
+        cout<<"Loading .";
+        sleep(1);
+        cout<<" .";
+        sleep(1);
+        cout<<" .";
+        sleep(1);
+
+        switch(choice){
+            case 1:
+                if(name.isfull())
+                {
+                    system("cls");
+                    cout<<"NO MORE SLOTS!\n";
+                    cout<<"Returning to main menu .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(1);
+                }
+                else
+                {
+                    system("cls");
+                    cout<<"R E S E R V I N G"<<endl<<endl;
+                    cout<<"Input full name here: ";
+                    cin.ignore();
+                    getline(cin, n);
+                    name.enqueue(n);
+                    cout<<"Input your year here: ";
+                    cin>>y;
+                    year.enqueue(y);
+                    cout<<"Input your course here: ";
+                    cin.ignore();
+                    getline(cin,c);
+                    course.enqueue(c);
+                    cout<<"Saving .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(1);
+                }
+                break;
+            case 2:
+                if (name.isempty())
+                {
+                    system("cls");
+                    cout<<"EMPTY!\n";
+                    cout<<"Returning to main menu .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(1);
+                }
+                else
+                {
+                    system("cls");
+                    cout<<"T R A N S C R I P T"<<endl<<endl;
+                    for(int i = 0; i<s; i++)
+                    {
+                        cout<<"Name: "<<name.dequeue(n)<<endl;
+                        cout<<"Year: "<<year.dequeue(y)<<endl;
+                        cout<<"Course: "<<course.dequeue(c)<<endl<<endl;
+                    }
+                    cout<<"Finishing up .";
+                    sleep(2);
+                    cout<<" .";
+                    sleep(1);
+                    cout<<" .";
+                    sleep(2);
+                }
+                break;
+        }
+
+    }while(choice>0);
     return 0;
 }
